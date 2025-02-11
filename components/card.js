1b0643c01098f48e824dde4e4eea2806065fc7de
Dmand723 <dawson.simmons723@gmail.com>
2025-02-11 12:59:55 -0700

    added saving favs to users data

--- components/card.js
+++ components/card.js
@@ -1,17 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import { FaRegStar } from "react-icons/fa";
 
 import { useContext } from "react";
 import { authContext } from "@/lib/api-handler/auth-contex";
+import { recipeContex } from "@/lib/api-handler/recipeHandler";
 
 export default function Card({ recipe, children, page = "user" }) {
   const { user } = useContext(authContext);
+  const { addFavRecipe } = useContext(recipeContex);
   const checkUserSavedRecipes = () => {};
+  const onSaveHandler = (r) => {
+    const data = {
+      id: r.id,
+      title: r.title,
+    };
+    addFavRecipe(user.uid, data);
+  };
   return (
     <div className="flex flex-col justify-center items-center">
       <div className=" flex flex-col my-5 mx-5 justify-center items-start bg-slate-600 rounded-lg shadow-lg">
         {page == "user" && user ? (
-          <FaRegStar className="mt-2 ml-2 size-[30px]" />
+          <FaRegStar
+            onClick={() => {
+              onSaveHandler(recipe);
+            }}
+            className="mt-2 ml-2 size-[30px] caret-rose-700 cursor-love"
+          />
         ) : (
           page == "user" &&
           !user && <small className="mt-2 ml-2">Please login in to save</small>
